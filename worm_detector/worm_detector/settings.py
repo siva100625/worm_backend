f7e014b2bc0849d8302d38ea585ec6ac9426b05f
siva100625 <153293464+siva100625@users.noreply.github.com>
2026-01-29 18:39:02 +0530

    Update settings.py

--- worm_detector/worm_detector/settings.py
+++ worm_detector/worm_detector/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9d7#5s&^t6v@f#30hn+c0(p7e_!vg^t)c&-4ek^9h9w0swx=r8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
